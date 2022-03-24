f7a0438facfbc0d591ff2a90fcba36ff8c02f452
hippo <72145776+hippole@users.noreply.github.com>
2022-03-24 11:42:21 -0400

    Update IodineGBACoreGlueCode.js

--- gba/user_scripts/IodineGBACoreGlueCode.js
+++ gba/user_scripts/IodineGBACoreGlueCode.js
@@ -188,7 +188,6 @@ var MixerInput = null;
 var timerID = null;
 window.onload = function () {
     if (!games[location.hash.substr(1)]) {
-	alert("Invalid Game Request! Redirecting to list of games.");
         window.location.href = "https://hippole.github.io/gba/invalidgame.html";
         return;
     }
